3769f8edf5f575186bdce2e0065df71138a362bb
gaurav210s <110066608+gaurav210s@users.noreply.github.com>
2024-09-01 02:15:47 +0530

    initial commit

---
+++ queries.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+username VARCHAR(15) UNIQUE NOT NULL,
+usercolor VARCHAR(15)
+);
+
+CREATE TABLE visited_countries(
+id SERIAL PRIMARY KEY,
+country_code CHAR(2) NOT NULL,
+user_id INTEGER REFERENCES users(id)
+);
+
+INSERT INTO users (name, color)
+VALUES ('name1', 'teal'), ('name2', 'pink');
+
+INSERT INTO visited_countries (country_code, user_id)
+VALUES ('FR', 1), ('GB', 1), ('CA', 2), ('FR', 2 );
+
+SELECT *
+FROM visited_countries
+JOIN users
+ON users.id = user_id;
